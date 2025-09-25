3062db4224013bef7eaa6e735593dabfb31f64a9
Ricardo Olazabal <ricardoolazabaljr@gmail.com>
2025-09-25 18:43:23 -0400

    create table queries

--- IMS.py
+++ IMS.py
@@ -8,6 +8,15 @@ import sqlite3
 from typing import Optional
 import uuid
 
+# QUERIES
+
+create_prod_table = "CREATE TABLE products(pID, name, description, quantity, price)";
+create_cat_table = "CREATE TABLE categories(cID, name, description)";
+create_sup_table = "CREATE TABLE suppliers(sID, name, contact)";
+create_img_table = "CREATE TABLE images(iID, pID, url)";
+create_catProd_table = "CREATE TABLE categoryProducts(cID, pID)";
+create_supProd_table = "CREATE TABLE supplierProducts(sID, pID)";
+
 # ------------------------- FUNCTIONS ---------------------------
 
 # Generate UUID given an optional input.
@@ -19,3 +28,11 @@ def gen_uuid(s: Optional[str] = None) -> str:
             raise ValueError(f"Invalid UUID provided: {s}")
     #Auto generate UUID if not provided input
     return str(uuid.uuid4())
+
+try:
+    with sqlite3.connect("IMS.db") as connection:
+        cursor = connection.cursor()
+
+        pass
+except sqlite3.OperationalError as error:
+    print("Failed to open database:", e)
