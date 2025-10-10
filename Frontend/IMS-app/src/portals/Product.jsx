da64d0e131c04901106f0a454474413194f10fa9
Ricardo Olazabal <ricardoolazabaljr@gmail.com>
2025-10-09 20:46:25 -0400

    input processing for product create

--- Frontend/IMS-app/src/portals/Product.jsx
+++ Frontend/IMS-app/src/portals/Product.jsx
@@ -1,53 +1,74 @@
+import { useState, useEffect } from 'react';
+
 function Product({fields, products}) {
 
-    const createProduct = () => {};
-    const handleCRUD = () => {};
+    const handleCreate = () => {
+        let inputs = fields;
+        for (let i = 0; i < inputs.length; i ++) {
+            inputs[i][1] = document.getElementById(inputs[i][0]).value;
+        }
+        console.log(inputs);
+    };
+
+    const handleRUD = (prod) => {
+        console.log(prod);
+    };
+
+    const [ choice, setChoice ] = useState(null);
+    useEffect(() => {
+        if (choice != null)
+            handleCRUD(choice);
+    }, [choice]);
 
     return (
         <>
             <h2>product portal</h2>
-            <form onSubmit={(e) => { e.preventDefault(); createProduct(); }}>
+            <form onSubmit={(e) => { e.preventDefault(); handleCreate(); }}>
                 {fields.map((data) => (
                     <>
                         <label>{"product " + data[0] + ":"}
-                            <input type={data[1]} name={data[0]} />
+                            <input type={data[1]} id={data[0]} />
                         </label>
                     </>
                 ))}
                 <button type="submit">create</button>
-                <p>validation message</p>
+                <p name="msg">validation message</p>
             </form>
-            <form onSubmit={(e) => { e.preventDefault(); handleCRUD(); }}>
+            <form onSubmit={(e) => { e.preventDefault(); }}>
                 <p>perform</p>
-                <li>
-                    <label>
-                        <input type="radio" name="crud" value="view" />view
-                    </label>
-                </li>
-                <li>
-                    <label>
-                        <input type="radio" name="crud" value="edit" />edit
-                    </label>
-                </li>
-                <li>
-                    <label>
-                        <input type="radio" name="crud" value="delete" />delete
-                    </label>
-                </li>
-                <li>
-                    <label>
-                        <input type="radio" name="crud" value="assoc" />associate
-                    </label>
-                </li>
-                <li>
-                    <label>
-                        <input type="radio" name="crud" value="diss" />dissassociate
-                    </label>
-                </li>
+                <ul>
+                    <li>
+                        <label>
+                            <input type="radio" name="rud" value="view" />view
+                        </label>
+                    </li>
+                    <li>
+                        <label>
+                            <input type="radio" name="rud" value="edit" />edit
+                        </label>
+                    </li>
+                    <li>
+                        <label>
+                            <input type="radio" name="rud" value="delete" />delete
+                        </label>
+                    </li>
+                    <li>
+                        <label>
+                            <input type="radio" name="rud" value="assoc" />associate
+                        </label>
+                    </li>
+                    <li>
+                        <label>
+                            <input type="radio" name="rud" value="diss" />dissassociate
+                        </label>
+                    </li>
+                </ul>
                 <p>on product:</p>
                 <ul>
                     {products.map((product) => (
-                        <li key={product}>{product}</li>
+                        <li key={product}>
+                            <button onClick={() => setChoice(product)}>{product}</button>
+                        </li>
                     ))}
                 </ul>
             </form>
