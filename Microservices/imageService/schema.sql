4831da9895fa5d2bb82078d9acb3d3c70b4fe666
VictorBCase <vwb6@case.edu>
2025-10-31 21:01:36 -0400

    change to image db

--- Microservices/imageService/schema.sql
+++ Microservices/imageService/schema.sql
@@ -2,7 +2,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS images (
     image_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    product_id UUID NOT NULL, 
+    product_id UUID NOT NULL REFERENCES products(product_id) ON DELETE CASCADE,, 
     url TEXT NOT NULL
 );
 
